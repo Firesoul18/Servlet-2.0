40a0aba82aa6a8be4b7baeebbda1d1cb4fce89ae
Yogesh Poonia <yogidarapoonia@gmail.com>
2024-06-20 13:20:53 +0530

    initial commit

---
+++ Servlet App/src/main/java/com/yogesh/AddServlet.java
@@ -0,0 +1,22 @@
+package com.yogesh;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		System.out.println("Sum is "+i+j);
+		try {
+			res.getWriter().println("Result is "+(i+j));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
